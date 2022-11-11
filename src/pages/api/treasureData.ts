272a3a4c12dace0018f8059a8c9fec0b5a503eb0
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2022-11-11 22:05:22 +0100

    update

--- src/pages/api/treasureData.ts
+++ src/pages/api/treasureData.ts
@@ -1,7 +1,8 @@
 import { readFileSync } from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
+import { join } from "path";
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-    const json = JSON.parse(readFileSync('data.json', 'utf-8'));
+    const json = JSON.parse(readFileSync(join('../../../data.json'), 'utf-8'));
     return res.json(json)
 }
