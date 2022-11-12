5754848323fb49bcf9bdf80040fea95b194086ab
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2022-11-12 18:16:02 +0100

    always have up to date copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 mt-10">
-            <span className="text-sm text-gray-500 sm:text-center">© 2022 <a href="https://supercrafter100.com/" className="hover:underline">Supercrafter100</a>. All Rights Reserved.</span>
+            <span className="text-sm text-gray-500 sm:text-center">© {new Date().getFullYear()} <a href="https://supercrafter100.com/" className="hover:underline">Supercrafter100</a>. All Rights Reserved.</span>
             <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 sm:mt-0">
                 <li>
                     <a href="https://www.planetminecraft.com/data-pack/mine-treasure/" className="mr-4 hover:underline md:mr-6">Datapack</a>
