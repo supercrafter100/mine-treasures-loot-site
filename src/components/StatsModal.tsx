f1a1f793dfe6460d833958867debf3f14fdd5813
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2022-11-15 21:54:04 +0100

    improve mobile responsiveness for modal

--- src/components/StatsModal.tsx
+++ src/components/StatsModal.tsx
@@ -16,13 +16,13 @@ const StatsModal = ({ item, setModal }: { item: MT_ITEM, setModal: Function }) =
     return (
         <>
             <div className='fixed top-0 left-0 h-screen w-screen modal-background' onClick={handleModalClick} style={{ backgroundColor: 'rgb(0, 0, 0, 0.7)' }}>
-                <div className="relative w-[80vw] h-[80vh] rounded-xl bg-gray-200 opacity-100 mx-[10vw] my-[10vh] p-5" onClick={handleModalClick}>
+                <div className="relative w-[80vw] h-[80vh] rounded-xl bg-gray-200 opacity-100 mx-[10vw] my-[10vh] p-5 overflow-x-auto" onClick={handleModalClick}>
                     <FontAwesomeIcon className="absolute top-5 left-5 text-2xl transition-all duration-100 hover:text-4xl" icon={faXmark} onClick={() => setModal(false)} />
                     {/* Grid */}
-                    <div className="w-full h-full grid grid-cols-3 gap-3">
+                    <div className="w-full h-full grid grid-cols-1 lg:grid-cols-3 gap-10 md:gap-3">
                         <section className="text-center">
                             <h1 className="text-xl md:text-4xl font-bold md:mb-5">{item.name ?? item.type.replace(/_/g, ' ')}</h1>
-                            <Image className="md:inline-block hidden md:visible " src={"/items/" + item.type + ".png"} alt={item.type} height={200} width={200} />
+                            <Image className="md:inline-block visible mx-auto " src={"/items/" + item.type + ".png"} alt={item.type} height={200} width={200} />
                             <section>
                                 {/* Lore */}
                                 {item.lore && item.lore.map((line, idx) => <p key={idx}>{line}</p>)}
@@ -34,7 +34,7 @@ const StatsModal = ({ item, setModal }: { item: MT_ITEM, setModal: Function }) =
                                 {item.attributes && item.attributes.map((attribute, idx) => <Attribute key={idx} attribute={attribute}></Attribute>)}
                             </div>
                         </section>
-                        <section className="text-center">
+                        <section className="text-center pb-5 md:pb-0">
                             <h1 className="text-xl md:text-4xl font-bold mb-5"><FontAwesomeIcon icon={faBook} /> Enchantments</h1>
                             <div className="grid grid-cols-1 gap-3">
                                 {item.unbreakable && <UnbreakableEnchantment />}
@@ -52,7 +52,7 @@ const Attribute = ({ attribute }: { attribute: MT_ATTRIBUTE }) => {
     const attributeData = attributeNames[attribute.name] || { name: attribute.name, type: "value" };
 
     return (
-        <div className="text-left">
+        <div className="text-center lg:text-left">
             <Image src="/items/armor_stand.png" alt="armor_stand" width={24} height={24} className="inline-block"></Image>
             <p className="text-lg md:text-xl inline-block align-middle pl-3">
                 {attribute.min !== attribute.max && (
@@ -82,7 +82,7 @@ const Attribute = ({ attribute }: { attribute: MT_ATTRIBUTE }) => {
 
 const Enchantment = ({ enchantment }: { enchantment: MT_ENCHANTMENT }) => {
     return (
-        <div className="text-left">
+        <div className="text-center lg:text-left">
             <Image src="/items/enchanted_book.png" alt="enchanted_book" width={24} height={24} className="inline-block"></Image>
             <p className="text-lg md:text-xl inline-block align-middle pl-3"><b>{enchantment.type.replace(/_/g, ' ')}</b> {enchantment.min}-{enchantment.max}</p>
         </div>
@@ -91,7 +91,7 @@ const Enchantment = ({ enchantment }: { enchantment: MT_ENCHANTMENT }) => {
 
 const UnbreakableEnchantment = () => {
     return (
-        <div className="text-left">
+        <div className="text-center lg:text-left">
             <Image src="/items/enchanted_book.png" alt="enchanted_book" width={24} height={24} className="inline-block"></Image>
             <p className="text-lg md:text-xl inline-block align-middle pl-3"><b>Unbreakable</b></p>
         </div>
