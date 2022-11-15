b749fab34248cadd4f7593ddea86d9799a492f3c
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2022-11-15 22:27:53 +0100

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { AppProps } from 'next/app'
+import Script from "next/script";
 
 import '../styles/index.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+    return <>
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+        <Script id="google-analytics" strategy="lazyOnload">
+            {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+        </Script>
+        <Component {...pageProps} />
+    </>
 }
 
 export default MyApp;
