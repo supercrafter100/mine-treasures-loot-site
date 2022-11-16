3dca027d296ff8b19838111ddb25d56dc6f629ce
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2022-11-16 20:04:47 +0100

    improve tooltip

--- src/components/HoverCard.tsx
+++ src/components/HoverCard.tsx
@@ -1,16 +1,12 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip'
 import { MT_ITEM } from '../interfaces'
 
-const HoverCard = ({ item, show }: { item: MT_ITEM, show: boolean }) => {
+const HoverCard = ({ item, id }: { item: MT_ITEM, id: string }) => {
     return (
-        <>
-            {show && (
-                <div className={"bg-gray-200 rounded-lg p-2 absolute bottom-[10] z-10"}>
-                    {item.lore.map((line) => <p>{line}</p>)}
-                </div>
-            )
-            }
-        </>
+        <ReactTooltip id={id} effect="solid" className="opacity-100">
+            {item.lore.map((line) => <p>{line}</p>)}
+        </ReactTooltip>
     )
 }
 
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -11,19 +11,20 @@ import HoverCard from './HoverCard'
 const Item = ({ item }: { item: MT_ITEM }) => {
 
     const [showModal, setShowModal] = useState(false);
-    const [showTooltip, setShowTooltip] = useState(false);
+    const id = (Math.random() + 1).toString(36).substring(2);
+
 
     const hasModal = item.attributes || item.enchantments;
 
     return (
         <div>
-            <div onClick={() => { setShowModal(true); setShowTooltip(false) }} className={hasModal && "cursor-pointer"} onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)}>
+            <div onClick={() => { setShowModal(true) }} className={hasModal && "cursor-pointer"} data-tip data-for={id} >
                 <Image className="inline-block" src={"/items/" + item.type + ".png"} alt={item.type} height={24} width={24} />
                 <span style={{ color: item.name ? rarityColors.legendary : 'black' }} className={"ml-5 align-middle"}>{item.name ?? item.type.replace(/_/g, ' ')}</span>
                 {hasModal && <FontAwesomeIcon className="align-middle inline-block ml-1 text-gray-500" icon={faArrowUpRightFromSquare} />}
             </div>
             {hasModal && showModal && <StatsModal item={item} setModal={setShowModal}></StatsModal>}
-            {item.lore && <HoverCard item={item} show={showTooltip}></HoverCard>}
+            {item.lore && <HoverCard item={item} id={id}></HoverCard>}
         </div>
     )
 }
