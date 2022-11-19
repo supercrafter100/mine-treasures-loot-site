91425ad46fa843cbac62632f82f5db91ba0e4c52
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2022-11-19 21:33:41 +0100

    new MT release

--- src/components/Locations.tsx
+++ src/components/Locations.tsx
@@ -2,8 +2,9 @@ import Image from 'next/image'
 import React from 'react'
 
 const Locations = ({ biomes }) => {
+    biomes = biomes.sort();
     return (
-        <div className="text-center">
+        <div className="text-center max-w-[70%] mx-auto">
             <span className="font-mono text-pink-500">{biomes.join(', ')}</span>
         </div>
     )
