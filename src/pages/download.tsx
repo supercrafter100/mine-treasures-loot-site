9f86d2c2c1b3e087e27c3cbded4e8308a8d2e1fa
Supercrafter100 <58982133+supercrafter100@users.noreply.github.com>
2023-01-16 12:03:58 +0100

    accidentally reverted 1.19.3 only, whoops

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -33,7 +33,7 @@ const download = () => {
                 <main className="mt-24">
                     <div className="mx-auto w-[70%] rounded-lg border border-blue-500 flex justify-between p-5 flex-wrap md:flex-nowrap">
                         <div className="text-center w-full text-2xl font-mono">
-                            <h1>Version 1.1.1 <em>(1.19+)</em></h1>
+                            <h1>Version 1.1.1 <em>(1.19.3 ONLY)</em></h1>
                             <button onClick={() => downloadFile('mine-treasure-1.1.1.zip')} className="bg-blue-500 rounded-lg px-5 py-2 mt-5">Download</button>
                         </div>
                         <div className="text-center w-full text-2xl font-mono">
